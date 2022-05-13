465825bc771bddef3df0e0805593cd4d638377c3
Rob Richard <rob@1stdibs.com>
2022-05-13 14:47:04 -0400

    Return underlying AsyncIterators when execute result is returned (#2843)
    # Conflicts:
    #	src/execution/execute.ts

--- src/execution/execute.ts
+++ src/execution/execute.ts
@@ -1737,6 +1737,7 @@ async function executeStreamIterator(
       label,
       path: fieldPath,
       parentContext,
+      iterator,
     });
 
     const dataPromise = executeStreamIteratorItem(
@@ -1779,6 +1780,7 @@ function yieldSubsequentPayloads(
   initialResult: ExecutionResult,
 ): AsyncGenerator<AsyncExecutionResult, void, void> {
   let _hasReturnedInitialResult = false;
+  let isDone = false;
 
   async function race(): Promise<IteratorResult<AsyncExecutionResult>> {
     if (exeContext.subsequentPayloads.length === 0) {
@@ -1849,19 +1851,31 @@ function yieldSubsequentPayloads(
           },
           done: false,
         });
-      } else if (exeContext.subsequentPayloads.length === 0) {
+      } else if (exeContext.subsequentPayloads.length === 0 || isDone) {
         return Promise.resolve({ value: undefined, done: true });
       }
       return race();
     },
-    // TODO: implement return & throw
-    // c8 ignore next 2
-    // will be covered in follow up
-    return: () => Promise.resolve({ value: undefined, done: true }),
-
-    // c8 ignore next 2
-    // will be covered in follow up
-    throw: (error?: unknown) => Promise.reject(error),
+    async return(): Promise<IteratorResult<AsyncExecutionResult, void>> {
+      await Promise.all(
+        exeContext.subsequentPayloads.map((asyncPayloadRecord) =>
+          asyncPayloadRecord.iterator?.return?.(),
+        ),
+      );
+      isDone = true;
+      return { value: undefined, done: true };
+    },
+    async throw(
+      error?: unknown,
+    ): Promise<IteratorResult<AsyncExecutionResult, void>> {
+      await Promise.all(
+        exeContext.subsequentPayloads.map((asyncPayloadRecord) =>
+          asyncPayloadRecord.iterator?.return?.(),
+        ),
+      );
+      isDone = true;
+      return Promise.reject(error);
+    },
   };
 }
 
